9e9336dc0041e22f6aa4ea9ec17cec9c397dd875
orivelton10 <orivelton10@gmail.com>
2018-03-08 11:49:42 -0300

    name file karma conf

--- karma.conf.js
+++ karma.conf.js
@@ -15,7 +15,7 @@ module.exports = function(config) {
     // list of files / patterns to load in the browser
     files: [
       'calculator/js/*.js',
-      'test/*.test.js'
+      'test/*.spec.js'
     ],
 
 
